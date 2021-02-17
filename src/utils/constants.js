bc0311dc65d4317de1c4e47bf60abcb4287e3d26
samuelndm <samuelnmoura@gmail.com>
2021-02-17 15:27:12 -0300

    refactor: change preview image size

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,14 +4,8 @@ export const URL_PARAMS = {
 
 export const IMAGE_SIZES = {
   ORIGINAL: "original",
-  WIDTH_100: "w100",
   WIDTH_200: "w200",
   WIDTH_300: "w300",
   WIDTH_400: "w400",
   WIDTH_500: "w500",
-  WIDTH_600: "w600",
-  WIDTH_700: "w700",
-  WIDTH_800: "w800",
-  WIDTH_900: "w900",
-  WIDTH_1000: "w1000",
 };
