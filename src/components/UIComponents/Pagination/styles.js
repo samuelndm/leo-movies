3e4f633f05599204099430f2421477aa242c1027
samuelndm <samuelnmoura@gmail.com>
2021-02-18 02:23:00 -0300

    style: small fix at pagination responsiveness

--- src/components/UIComponents/Pagination/styles.js
+++ src/components/UIComponents/Pagination/styles.js
@@ -5,6 +5,7 @@ export const Container = styled.div`
   width: 100%;
   max-width: 100%;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   overflow: hidden;
