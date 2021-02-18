06422b78aee785b6cc4501d2b773af0490eb97fe
samuelndm <samuelnmoura@gmail.com>
2021-02-18 10:11:52 -0300

    feat: add sort_by types

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,23 @@ export const URL_PARAMS = {
   SEARCH: "search",
 };
 
+export const SORT_BY = {
+  POPULARITY_ASC: "popularity.asc",
+  POPULARITY_DESC: "popularity.desc",
+  RELEASE_DATE_ASC: "release_date.asc",
+  RELEASE_DATE_DESC: "release_date.desc",
+  REVENUE_ASC: "revenue.asc",
+  REVENUE_DESC: "revenue.desc",
+  PRIMARY_RELEASE_DATE_ASC: "primary_release_date.asc",
+  PRIMARY_RELEASE_DATE_DESC: "primary_release_date.desc",
+  TITLE_ASC: "title.asc",
+  TITLE_DESC: "title.desc",
+  VOTE_AVERAGE_ASC: "vote_average.asc",
+  VOTE_AVERAGE_DESC: "vote_average.desc",
+  VOTE_COUNT_ASC: "vote_count.asc",
+  VOTE_COUNT_DESC: "vote_count.desc",
+};
+
 export const IMAGE_SIZES = {
   ORIGINAL: "original",
 
