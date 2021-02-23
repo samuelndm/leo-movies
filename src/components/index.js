ac8fb664d6066d683418ed9966e6811bbed68882
samuelndm <samuelnmoura@gmail.com>
2021-02-23 02:47:02 -0300

    refactor: renamed UIComponents to UI

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export * from "./Movies";
 export * from "./People";
 export * from "./TvShows";
-export * as UI from "./UIComponents";
+export * as UI from "./UI";
 export * as Card from "./Cards";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as PreviewsSlider } from "./PreviewsSlider/PreviewsSlider";
